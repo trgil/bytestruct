dd50658b330355c4c2476798cc76c63964800d93
Gil Treibush <tkks085@gmail.com>
2026-02-06 22:07:27 +0200

    Add setup and requirements files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bytestruct',
+    version='1.0.0-alpha.1',
+    description='A declarative Python library for describing, parsing, and manipulating binary data layouts',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gil Treibush',
+    url='https://github.com/trgil/bytestruct',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools>=42',  # Specify version if needed
+    ],
+    extras_require={
+        'cli': [            # For running standalone
+        ],
+        'dev': [            # For development
+            'pytest',       # For testing
+            'flake8',       # For linting
+            'sphinx',       # For documentation
+        ],
+    },
+    python_requires='>=3.6',
+)
